e2f05ee7e05bbe76d82f132673db37f1302b37b6
keyrun <kiranpatil1999@outlook.com>
2023-04-05 02:14:28 +0530

    React

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import './index.css'
+const author = "Dav Pilkey"
+const imageAlt = "Dog Man: Twenty Thousand Fleas Under the Sea"
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const inlineHeadStyle =
+{
+    color: '#617d98',
+    fontsize: '0.75',
+    marginTop: '0.5rem'
+}
+
+const BookList = () => {
+    return <section className="booklist">
+        <Book title="Atomic Habits: The life-changing" image="https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/51-nXsSRfZL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg" author="James Clear" />
+        <Book title="Ikigai: The Japanese secret to a long and happy life" image="https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/51xwGSNX-EL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg" author="Hector Garcia" />
+        <Book title="The Psychology of Money" image="https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/41r6F2LRf8L._SY264_BO1,204,203,200_QL40_FMwebp_.jpg" author="Morgan Housel" />
+
+    </section>
+}
+const Book = (props) => {
+    return <article className="books">
+        <img src={props.image} alt={imageAlt}></img>
+        <h2>{props.title}</h2>
+        <h4 style={inlineHeadStyle}>{props.author}</h4>
+
+
+    </article>
+}
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<BookList />);
+
